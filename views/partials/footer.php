6be26e05f9015e91077d9ab6395e8e418f451065
Justin Dietert <justin.dietert@gmail.com>
2015-04-17 16:32:54 -0500

    added site partials (header, footer, nav) and basic styling

---
+++ views/partials/footer.php
@@ -0,0 +1,14 @@
+    <footer class="row">
+        <div class="small-12 columns">
+
+            <hr/>
+
+            <div class="row">
+                <div class="small-10 columns">
+                    <p class="footer-text">© Copyright 2015 Adlister.</p>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
