c6f051880cc2dae9f70b0a91b3ff0740acaae463
Jamie Haskell <jmehaskell@gmail.com>
2015-04-17 16:40:00 -0500

    created basic hard-coded html for ad lists

---
+++ public/ads.index.php
@@ -0,0 +1,65 @@
+	<div class="ads">
+		<h3>2005 Chrysler Sebring</h3>
+		<img src="img/myVehicle.jpg">
+		<p>Description: Good condition, minor body work, no mechanical problems.</p>
+		<p>Price: $2,500</p>
+		<p>Contact Jamie if interested:
+			<ul>
+				<li>Email: email@gmail.com</li>
+				<li>Cell: 555-555-5555</li>
+			</ul>
+		</p>
+	</div>
+	<hr>
+	<div class="ads">
+		<h3>2005 Chrysler Sebring</h3>
+		<img src="img/myVehicle.jpg">
+		<p>Description: Good condition, minor body work, no mechanical problems.</p>
+		<p>Price: $2,500</p>
+		<p>Contact Jamie if interested:
+			<ul>
+				<li>Email: email@gmail.com</li>
+				<li>Cell: 555-555-5555</li>
+			</ul>
+		</p>
+	</div>
+	<hr>
+	<div class="ads">
+		<h3>2005 Chrysler Sebring</h3>
+		<img src="img/myVehicle.jpg">
+		<p>Description: Good condition, minor body work, no mechanical problems.</p>
+		<p>Price: $2,500</p>
+		<p>Contact Jamie if interested:
+			<ul>
+				<li>Email: email@gmail.com</li>
+				<li>Cell: 555-555-5555</li>
+			</ul>
+		</p>
+	</div>
+	<hr>
+	<div class="ads">
+		<h3>2005 Chrysler Sebring</h3>
+		<img src="img/myVehicle.jpg">
+		<p>Description: Good condition, minor body work, no mechanical problems.</p>
+		<p>Price: $2,500</p>
+		<p>Contact Jamie if interested:
+			<ul>
+				<li>Email: email@gmail.com</li>
+				<li>Cell: 555-555-5555</li>
+			</ul>
+		</p>
+	</div>
+	<hr>
+	<div class="ads">
+		<h3>2005 Chrysler Sebring</h3>
+		<img src="img/myVehicle.jpg">
+		<p><Description:</strong> Good condition, minor body work, no mechanical problems.</p>
+		<p>Price: $2,500</p>
+		<p>Contact Jamie if interested:
+			<ul>
+				<li>Email: email@gmail.com</li>
+				<li>Cell: 555-555-5555</li>
+			</ul>
+		</p>
+	</div>
+	<hr>
