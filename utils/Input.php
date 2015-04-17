e02d9eb6534bfbc82f9cd5817f1e8f3c86027246
Jamie Haskell <jmehaskell@gmail.com>
2015-04-17 13:12:59 -0500

    initializing repo

---
+++ utils/Input.php
@@ -0,0 +1,105 @@
+<?php
+
+class Input
+{
+    /**
+     * Check if a given value was passed in the request
+     *
+     * @param string $key index to look for in request
+     * @return boolean whether value exists in $_POST or $_GET
+     */
+    public static function has($key)
+    {
+        return isset($_REQUEST[$key]) ? true : false;
+    }
+
+    /**
+     * Get a requested value from either $_POST or $_GET
+     *
+     * @param string $key index to look for in index
+     * @param mixed $default default value to return if key not found
+     * @return mixed value passed in request
+     */
+    public static function get($key, $default = NULL)
+    {
+        return isset($_REQUEST[$key]) ? $_REQUEST[$key] : $default; 
+    }
+
+    public static function getString($key, $min = NULL, $max = NULL)
+    {
+        $keyValue = self::get($key);
+
+        if(empty($key)){
+            throw new OutOfRangeException("A key was not provided.");
+        }
+
+        if(!is_string($key)) {
+            throw new InvalidArgumentException("$key must be a string.");
+        }
+
+        if(isset($min) && isset($max) && (!is_int($max) || !is_int($min))){
+            throw new InvalidArgumentException("Minimum and Maximum must be integers.");
+        }
+
+        if(!is_string($keyValue) || !isset($keyValue) || is_numeric($keyValue)){
+            throw new DomainException("$key input must be a string!");
+        }
+
+        if(strlen($keyValue) < $min || strlen($keyValue) > $max){
+            throw new LengthException("$key input must be between $min and $max characters long.");
+        }
+
+        return trim($keyValue);
+    }
+
+    public static function getNumber($key, $min = NULL, $max = NULL)
+    {
+        $keyValue = trim(self::get($key));
+
+        if(empty($key)){
+            throw new OutOfRangeException("A key was not provided.");
+        }
+
+        if(!is_string($key)) {
+            throw new InvalidArgumentException("$key must be a string.");
+        }
+
+        if(isset($min) && isset($max) && (!is_int($max) || !is_int($min))){
+            throw new InvalidArgumentException("Minimum and Maximum must be integers.");
+        }
+
+        if(!is_numeric($keyValue) || !isset($keyValue)){
+            throw new exception ("Key $key must be a number!");
+        }
+
+        if(isset($min) && isset($max) && (strlen($keyValue) < $min || strlen($keyValue) > $max)){
+            throw new RangeException("$key input must be between $min and $max.");
+        }
+
+        return (float)$keyValue;
+    }
+
+    public static function getDate($key)
+    {
+        // Re-format the user inputted date to the correct format, using PHP library functions, before passing to MySQL 
+        // If date field is left blank by the user, default to today's date
+        if (self::get($key)) {
+            $userDate = date_create(trim(self::get($key)));
+            if (!$userDate) {
+                throw new exception ("Invalid date; must be in the format: YYYY-MM-DD."); 
+            } else {
+               return $newDate = date_format($userDate, 'Y-m-d');
+            }
+        } else {
+            return $newDate = date('Y-m-d');
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                      DO NOT EDIT ANYTHING BELOW!!                     //
+    // The Input class should not ever be instantiated, so we prevent the    //
+    // constructor method from being called. We will be covering private     //
+    // later in the curriculum.                                              //
+    ///////////////////////////////////////////////////////////////////////////
+    private function __construct() {}
+}
---
+++ utils/Log.php
@@ -0,0 +1,81 @@
+<?php
+	// codeup.dev/login2.php is an example file that uses this class
+	class Log
+	{
+		protected $filename; 
+		private $handle;
+
+		// This function runs automatically whenever the class is instantiated
+		// It's job is to call the filename and handle to be 'set'
+		public function __construct($prefix='log')
+		{
+			$this->setFilename($prefix);
+			$this->setHandle();
+		}
+		
+		// Sets the protected filename when the class is instantiated, via the __construct() method 
+		protected function setFilename($prefix)
+		{
+			// Set timezone and date format
+		    date_default_timezone_set('America/Chicago');
+			$date = date('Y-m-d');
+
+			if(is_string($prefix)) {
+				$this->filename = "{$prefix}-{$date}.log";
+			} else {
+				$this->filename = "log-{$date}.log";
+			}
+		}
+
+		// Allows access to the filename, but not the ability to alter it
+		public function getFilename()
+		{
+			return $this->filename;
+		}
+
+		// Sets the private handle when the class is insantiated, via the __construct() method
+		protected function setHandle()
+		{
+			$this->handle = fopen($this->filename, 'a+');
+		}
+
+		// Allows access to the handle, but not the ability to alter it
+		public function getHandle()
+		{
+			return $this->handle;
+		}
+
+		// Appends a message to the end of the contents of the file
+		public function logMessage($logLevel, $message)
+		{
+		    // Set timezone and date format
+		    // date_default_timezone_set('America/Chicago');
+			// $date = date('Y-m-d');
+		    
+		    // Assign a file to the filename property inside the instance of the Log class
+			// $this->filename = "log {$date}.log";
+		    // $handle = fopen($this->filename, 'a+');
+		    fwrite($this->handle, PHP_EOL.date("Y-m-d H:i:s") . " [$logLevel] $message");
+		    // fclose($handle);
+		}
+
+		// Sends a 'logLevel' and message to be appended to the file
+		public function logInfo($message)
+		{
+			$this->logMessage("INFO", $message);
+		}
+
+		// Sends a 'logLevel' and message to be appended to the file
+		public function logError($message)
+		{
+			$this->logMessage("ERROR", $message);
+		}
+
+		// This function runs automatically whenever the class is instantiated
+		// It's job is to close the open file that was opened and altered by the methods above
+		public function __destruct()
+		{
+			fclose($this->getHandle());
+		}
+	}
+?>
