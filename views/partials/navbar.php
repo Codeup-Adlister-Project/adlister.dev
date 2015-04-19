1f3bf0340afb252f94b3aa4788428f467b6dfd2a
Justin Dietert <justin.dietert@gmail.com>
2015-04-18 23:53:30 -0500

    updates to content for index.php, footer, and navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,5 +1,5 @@
-    <div class="contain-to-grid fixed">
-        <nav class="top-bar" data-topbar>
+    <div class="fixed">
+        <nav class="top-bar" data-topbar data-options="scrolltop:false">
             <ul class="title-area">
                 <li class="name">
                     <h1 id="site-name"><a href="/"><i class="fa fa-rocket"></i> ad<span class="logo-liste">liste</span>r</a></h1>
@@ -10,9 +10,16 @@
             <section class="top-bar-section">
                 <!-- Right Nav Section -->
                 <ul class="right">
-                    <li class="active"><a href="#">Featured ads</a></li>
+                    <li class="divider show-for-small-only"></li>
+                    <li class="active"><a href="#featured">Featured ads</a></li>
+                    <li class="divider show-for-small-only"></li>
+                    <li class=""><a href="#">All ads</a></li>
+                    <li class="divider show-for-small-only"></li>
                     <li class=""><a href="#">Categories</a></li>
-                    <li class=""><a href="#">Contact</a></li>
+                    <li class="divider show-for-small-only"></li>
+                    <li class=""><a href="#">Sign up</a></li>
+                    <li class="divider"></li>
+                    <li class=""><a href="#">Login</a></li>
                 </ul>
             </section>
         </nav>
