365bee80a85ed770186dd58b810bdc73f41a8ecb
Justin Dietert <justin.dietert@gmail.com>
2015-04-18 23:50:14 -0500

    updated html structure of index ads

---
+++ public/ads.index.php
@@ -0,0 +1,95 @@
+    
+    <section id="featured">
+        
+        <div class="row">
+            <div class="small-12 columns">
+                <h5 class="featured-ads">Featured ads</h5>
+            </div>
+        </div>   
+
+        <div class="row">
+            <div class="large-4 medium-6 columns">       
+                <div class="ad">
+                    <div class="panel">
+                        <h3>2005 Chrysler Sebring</h3>
+                        <img src="http://lorempixel.com/600/400/transport/">
+                        <p>Description: Good condition, minor body work, no mechanical problems.</p>
+                        <p>Price: $2,500</p>
+                        <p>Contact Jamie if interested:
+                            <ul>
+                                <li>Email: email@gmail.com</li>
+                                <li>Cell: 555-555-5555</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="large-4 medium-6 columns">    
+                <div class="ad">
+                    <div class="panel">
+                        <h3>2005 Chrysler Sebring</h3>
+                        <img src="http://lorempixel.com/600/400/transport/">
+                        <p>Description: Good condition, minor body work, no mechanical problems.</p>
+                        <p>Price: $2,500</p>
+                        <p>Contact Jamie if interested:
+                            <ul>
+                                <li>Email: email@gmail.com</li>
+                                <li>Cell: 555-555-5555</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="large-4 medium-6 columns">
+                <div class="ad">
+                    <div class="panel">
+                        <h3>2005 Chrysler Sebring</h3>
+                        <img src="http://lorempixel.com/600/400/transport/">
+                        <p>Description: Good condition, minor body work, no mechanical problems.</p>
+                        <p>Price: $2,500</p>
+                        <p>Contact Jamie if interested:
+                            <ul>
+                                <li>Email: email@gmail.com</li>
+                                <li>Cell: 555-555-5555</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="large-4 medium-6 columns">
+                <div class="ad">
+                    <div class="panel">
+                        <h3>2005 Chrysler Sebring</h3>
+                        <img src="http://lorempixel.com/600/400/transport/">
+                        <p>Description: Good condition, minor body work, no mechanical problems.</p>
+                        <p>Price: $2,500</p>
+                        <p>Contact Jamie if interested:
+                            <ul>
+                                <li>Email: email@gmail.com</li>
+                                <li>Cell: 555-555-5555</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="large-4 medium-6 columns end">
+                <div class="ad">
+                    <div class="panel">
+                        <h3>2005 Chrysler Sebring</h3>
+                        <img src="http://lorempixel.com/600/400/transport/">
+                        <p>Description: Good condition, minor body work, no mechanical problems.</p>
+                        <p>Price: $2,500</p>
+                        <p>Contact Jamie if interested:
+                            <ul>
+                                <li>Email: email@gmail.com</li>
+                                <li>Cell: 555-555-5555</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div> 
+        </div>  
+
+
+    </section>
