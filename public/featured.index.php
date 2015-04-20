a585696c6762aa5688fdc7c1a32c1d881c9a43e4
Justin Dietert <justin.dietert@gmail.com>
2015-04-20 13:58:15 -0500

    added View All Ads link

--- public/featured.index.php
+++ public/featured.index.php
@@ -52,7 +52,7 @@
                     </div>
                 </div>
             </div>
-            <div class="large-4 medium-6 columns end">
+            <div class="large-4 medium-6 columns">
                 <div class="ad">
                     <div class="panel">
                         <h3><a href="ads.show.php">2005 Chrysler Sebring</a></h3>
@@ -60,6 +60,13 @@
                         <p><span class="pre">Description</span><span class="description">Good condition, minor body work, no mechanical problems.</span></p>
                         <p><span class="pre">Price</span><span class="price">$2,500</span></p>
                         <p class="view-ad"><a href="ads.show.php">View <i class="fa fa-chevron-circle-right"></i></a></p>
+                    </div>
+                </div>
+            </div> 
+            <div class="large-4 medium-6 columns end">
+                <div class="ad ad-disabled">
+                    <div class="panel panel-disabled">
+                        <p class="view-ad"><a href="ads.index.php">View all ads <i class="fa fa-chevron-circle-right"></i></a></p>
                     </div>
                 </div>
             </div> 
