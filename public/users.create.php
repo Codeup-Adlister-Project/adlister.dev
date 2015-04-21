006b00dbd55cc9de2b42cdc1b7a0fb979206c34b
Justin Dietert <justin.dietert@gmail.com>
2015-04-20 21:34:31 -0500

    defined form styles, implemented fully on ads.edit.php

--- public/users.create.php
+++ public/users.create.php
@@ -24,24 +24,32 @@
 
 <?php require_once '../views/partials/navbar.php'; ?>
 
-<h2>Create an Account</h2>
-<form method="POST" action=''>
-	<p>
-		<input type='text' name='username' value='' placeholder='Username' required></input>
-	</p>
-	<p>
-		<input type='password' name='password' value='' placeholder='Password' required></input>
-	</p>
-	<p>
-		<input type='password' name='confirmPass' value='' placeholder='Confirm Password' required></input>
-	</p>
-	<p>
-		<input type='text' name='email' value='' placeholder='Email' required></input>
-	</p>
-		<input type='submit' name='submit' value='Submit'></input>
+<section class="form">
+    <div class="row">
+        <div class="small-12 columns">
+            <h2>Create an Account</h2>
+            <form method="POST" action=''>
+            	<p>
+            		<input type='text' name='username' value='' placeholder='Username' required></input>
+            	</p>
+            	<p>
+            		<input type='password' name='password' value='' placeholder='Password' required></input>
+            	</p>
+            	<p>
+            		<input type='password' name='confirmPass' value='' placeholder='Confirm Password' required></input>
+            	</p>
+            	<p>
+            		<input type='text' name='email' value='' placeholder='Email' required></input>
+            	</p>
+            		<input type='submit' name='submit' value='Submit'></input>
 
 
-<!-- Need to add an image-upload feature for profile photo here -->
+            <!-- Need to add an image-upload feature for profile photo here -->
+
+            </form>
+        </div>
+    </div>
+</section>
 
 <?php require_once '../views/partials/footer.php'; ?>
     <script src="/js/vendor/jquery.js"></script>
@@ -52,4 +60,3 @@
 </body>
 </html>
 	
-</form>
