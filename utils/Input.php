401b81999c440c5dc8024403abaa76c7a86a92c3
Jamie Haskell <jmehaskell@gmail.com>
2015-04-22 16:19:19 -0500

    added an email validation method

--- utils/Input.php
+++ utils/Input.php
@@ -95,6 +95,39 @@ class Input
         }
     }
 
+    public static function validateEmail($key)    // Found on stackoverflow: 'Best email validation function'
+    {
+        $email = self::get($key);
+
+        // First, we check that there's one @ symbol, and that the lengths are right
+        if (!preg_match("/^[^@]{1,64}@[^@]{1,255}$/", $email)) {
+            // Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
+            throw new Exception ('Invalid email format');
+        }
+        // Split it into sections to make life easier
+        $email_array = explode("@", $email);
+        $local_array = explode(".", $email_array[0]);
+        for ($i = 0; $i < sizeof($local_array); $i++) {
+            if (!preg_match("/^(([A-Za-z0-9!#$%&'*+\/=?^_`{|}~-][A-Za-z0-9!#$%&'*+\/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$/", $local_array[$i])) {
+                throw new Exception ('Invalid email format');
+            }
+        }
+        if (!preg_match("/^\[?[0-9\.]+\]?$/", $email_array[1])) { // Check if domain is IP. If not, it should be valid domain name
+            $domain_array = explode(".", $email_array[1]);
+            if (sizeof($domain_array) < 2) {
+                throw new Exception ('Invalid email domain'); // Not enough parts to domain
+            }
+            for ($i = 0; $i < sizeof($domain_array); $i++) {
+                if (!preg_match("/^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$/", $domain_array[$i])) {
+                    throw new Exception ('Invalid email format');
+                }
+            }
+        }
+
+        return true;
+     
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
