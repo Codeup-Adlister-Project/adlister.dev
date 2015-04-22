15ea9fb3f56ae03d638d4468b7eefb5b15abe662
Jamie Haskell <jmehaskell@gmail.com>
2015-04-22 09:22:52 -0500

    just added two comments

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -19,7 +19,7 @@
 <!-- Log-in Pop-up Modal Window -->
     <div id="loginModal" class="reveal-modal small" data-reveal aria-labelledby="login" aria-hidden="true" role="dialog">
 
-        // If not logged in, require the log-in form inside the modal
+        <!-- If not logged in, require the log-in form inside the modal -->
         <?php if(!Auth::check()){
                 require_once 'auth.login.php'; 
             }
