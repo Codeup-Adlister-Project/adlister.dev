1a359a1fb7651dd3888f0730eac8ce074cb9b294
Jamie Haskell <jmehaskell@gmail.com>
2015-04-22 16:32:59 -0500

    removed unnecessary require

--- public/ads.index.php
+++ public/ads.index.php
@@ -1,6 +1,5 @@
 <?php 
     require_once($_SERVER['DOCUMENT_ROOT'].'../../bootstrap.php');
-    require_once($_SERVER['DOCUMENT_ROOT'].'../../models/Ad.php');
 
     function pageController()
     {
