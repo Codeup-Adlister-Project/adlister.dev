aa609c0b755315104a42a765934e5fed11742bc7
Jamie Haskell <jmehaskell@gmail.com>
2015-04-23 16:30:14 -0500

    refactored: removed foreach loop in html and instead replaced it with a call to Ad::find() at the top

--- public/ads.show.php
+++ public/ads.show.php
@@ -1,15 +1,15 @@
 <!-- Shows a single ad that has been clicked from the ads.index.php-->
 
-<?php 
+<?php
     require_once($_SERVER['DOCUMENT_ROOT'].'../../bootstrap.php');
-    require_once($_SERVER['DOCUMENT_ROOT'].'../../models/Ad.php');
+
 
     function pageController()
     {
         $data =['allAds' => Ad::all()];
-        return $data;   
+        return $data;
     }
-    
+
     extract(pageController());  // $allAds will now represent all the records from the database
 
     // Retrieve record with id matching the $_GET request in url
@@ -32,24 +32,24 @@
 
 <section id="show">
     <div class="row">
-    	<div class="medium-8 columns">       
+        <div class="medium-8 columns">
             <div class="post">
                 <div class="panel view">
-					<h3><?= $ad['title']; ?></h3>
-				    <a href="#"><img src="<?= $ad['image_url']; ?>"></a>
-				    <p><span class="pre">Description</span><span class="description"><?= $ad['description']; ?></span></p>
-				    <p><span class="pre">Price</span><span class="price">$<?= $ad['price']; ?></span></p>
-				    <p>Contact <span class="contact"><?= $ad['contact_name']; ?></span> if interested: </p>
-			        <ul>
-			            <li><span class="pre">Email</span><?= $ad['contact_email']; ?></li>
-			            <li><span class="pre">Phone</span><?= $ad['contact_phone']; ?></li>
-			        </ul>
+                    <h3><?= $ad['title']; ?></h3>
+                    <a href="#"><img src="<?= $ad['image_url']; ?>"></a>
+                    <p><span class="pre">Description</span><span class="description"><?= $ad['description']; ?></span></p>
+                    <p><span class="pre">Price</span><span class="price">$<?= $ad['price']; ?></span></p>
+                    <p>Contact <span class="contact"><?= $ad['contact_name']; ?></span> if interested: </p>
+                    <ul>
+                        <li><span class="pre">Email</span><?= $ad['contact_email']; ?></li>
+                        <li><span class="pre">Phone</span><?= $ad['contact_phone']; ?></li>
+                    </ul>
                         <!-- If the currently displayed ad is the last one in the database: -->
-                        <?php if(count($allAds) == $_GET['id']) { ?>
+                        <?php if($ad['id'] == 1) { ?>
                                 <p class="view-post"><a href="ads.index.php">Back to all ads <i class="fa fa-chevron-circle-right"></i></a></p>
                         <?php } else { ?>
                                 <p class="view-post"><a href="ads.show.php?id=<?= $ad['id']-1; ?>">View next ad <i class="fa fa-chevron-circle-right"></i></a></p>
-					   <?php } ?>
+                       <?php } ?>
                 </div>
             </div>
         </div>
