8257be5fac5dcbdbc2b7473e6a9364bd9db48aae
Justin Dietert <justin.dietert@gmail.com>
2015-04-24 09:34:16 -0500

    added quickview to ads.index and updated view ad buttons

--- public/featured.index.php
+++ public/featured.index.php
@@ -30,8 +30,8 @@
                             <img src="<?= $ad['image_url']; ?>" alt="No image provided.">
                             <p><span class="pre">Description</span><span class="description"><?= $ad['description']; ?></span></p>
                             <p><span class="pre">Price</span><span class="price">$<?= $ad['price']; ?></p>
-                            <p class="view-post"><a href="ads.show.php?id=<?= $ad['id']; ?>">View Entire Ad<i class="fa fa-chevron-circle-right"></i></a></span></p>
-                            <p class="view-post"><a data-dropdown="<?= 'drop' . $ad['id']; ?>" aria-controls="drop2" aria-expanded="false">Quick View<i class="fa fa-chevron-circle-right"></i></a></span></p>
+                            <p class="view-post"><a href="ads.show.php?id=<?= $ad['id']; ?>">View Ad <i class="fa fa-chevron-circle-right"></i></a></span></p>
+                            <p class="view-post"><a data-dropdown="<?= 'drop' . $ad['id']; ?>" aria-controls="drop2" aria-expanded="false">Quick View <i class="fa fa-chevron-circle-right"></i></a></span></p>
                         </div>
                         <div id="<?= 'drop' . $ad['id']; ?>" data-dropdown-content class="f-dropdown content panel view" aria-hidden="true" tabindex="-1">
                             <p>Contact <span class="contact"><?= $ad['contact_name']; ?></span> if interested: </p>
